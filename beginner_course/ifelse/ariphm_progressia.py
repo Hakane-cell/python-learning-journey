e485ccb0646101cd521bcd2f95eff6a06e10b54c
Hakane-cell <anastastatakidze@gmail.com>
2025-11-17 16:59:05 +0500

    Add files via upload

---
+++ beginner_course/ifelse/ariphm_progressia.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+d = c - b
+if c == a + d*(3-1):
+    print('YES')
+else:
+    print('NO')
---
+++ beginner_course/ifelse/password.py
@@ -0,0 +1,7 @@
+first_answer = input()
+second_answer = input()
+
+if first_answer == second_answer:
+    print("Пароль принят")
+else:
+    print("Пароль не принят")
